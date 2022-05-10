29eaca6408040f26991448b5bec7ed2688be3ab6
Rajveer0710 <90354367+Rajveer0710@users.noreply.github.com>
2022-05-10 06:52:00 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,35 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "JAVASCRIPT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,50)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,70)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("A S V J A R T I P C", 700,250)
+    fill("lightblue")
+    text("Hint: The code this game runs on!!", 700,270)
+
+    fill("white")
+    textSize(15)
+    text("Solve the Code to Unlock the Treasure!", 650, 475)
+}
